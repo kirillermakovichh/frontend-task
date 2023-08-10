a333457eeb9f2d2a32a2313e4799dd2f84ca618e
Kirill Ermakovich <106441917+kirillermakovichh@users.noreply.github.com>
2023-08-10 22:23:47 +0300

    Update RemoveBtn.jsx

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -9,7 +9,7 @@ export default function RemoveBtn({ id }) {
     const confirmed = confirm("Are you sure?");
 
     if (confirmed) {
-      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+      const res = await fetch(`https://frontend-task-sigma.vercel.app/api/topics?id=${id}`, {
         method: "DELETE",
       });
 
