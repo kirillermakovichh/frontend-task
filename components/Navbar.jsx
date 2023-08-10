f5fa2ff05b825c0d737cd499c5552609686e2826
Kirill <zeron2001@gmail.com>
2023-08-10 23:30:18 +0300

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,19 +4,24 @@ import { getServerSession } from "next-auth/next";
 
 export default async function Navbar() {
   const session = await getServerSession(options);
-  console.log(session);
+
   return (
     <nav className="flex justify-between items-center bg-teal-800 px-8 py-4 rounded sm:px-3">
       <div>
-        <Link className="text-white font-bold mr-4 sm:text-black sm:bg-white sm:rounded-full sm:py-1 sm:px-2 sm:mr-2" href={"/"}>
-          E<span className="sm:hidden">rmakovich</span> 
-        </Link>
         <Link
-          className="bg-green-500 px-3 py-2 rounded-full text-white font-bold"
-          href={"/addTopic"}
+          className="text-white font-bold mr-4 sm:text-black sm:bg-white sm:rounded-full sm:py-1 sm:px-2 sm:mr-2"
+          href={"/"}
         >
-          Add Topic
+          E<span className="sm:hidden">rmakovich</span>
         </Link>
+        {session && (
+          <Link
+            className="bg-green-500 px-3 py-2 rounded-full text-white font-bold"
+            href={"/addTopic"}
+          >
+            Add Topic
+          </Link>
+        )}
       </div>
       <div className="flex items-center">
         <span className="text-white sm:hidden">{session?.user.name}</span>
